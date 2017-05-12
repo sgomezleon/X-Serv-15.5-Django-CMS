3245ced1441be95ace6ef1f231d3275adb783872
sgomezleon <sergom19@hotmail.com>
2017-05-12 14:10:54 +0200

    Django-cms terminado

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from cms.models import Pages
 
 # Create your views here.
+
+def home(request):
+	base_datos = Pages.objects.all()
+	respuesta = "<h1>" + "Contenidos" + "</h1>"
+	respuesta += "<ol>"
+	for pag in base_datos:
+		respuesta +=  "<li><a href='" + str(pag.id) +"'>" + pag.name + "</a>" + " " + pag.page 
+	respuesta += "</ol>"
+	return HttpResponse(respuesta)
+
+def numero_pagina(request, num):
+	try:
+		page = Pages.objects.get(id = num)
+		respuesta = page.page
+	except Pages.DoesNotExist:
+		respuesta = "Pagina no encontrada"
+	return HttpResponse(respuesta)
